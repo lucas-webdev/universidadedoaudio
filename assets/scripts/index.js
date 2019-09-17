d6529793c79e1492db3a8ad80f2d2f1a9b9d2eb6
Lucas Medeiros <lucas.medeiros@dito.com.br>
2019-09-17 09:29:23 -0300

    Apply basic banner transition

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,11 +1,12 @@
-import mentores from "./objects.js";
+// import mentores from "./objects.js";
 $(() => {
-    $('.banners-control li').bind('click', (event) => {
-        const $anchor = $(this).attr('href');
-
-        $('.banners-control').stop().animate({
-            scrollLeft: $($anchor).offset().left
-        }, 1000);
-        event.preventDefault();
+  [].forEach.call(document.querySelectorAll(".mentor-banner-navlink"), function(el) {
+    el.addEventListener("click", function(e) {
+      e.preventDefault();
+      const anchor = $(this).attr("href");
+      console.log(anchor);
+      $(".mentor-banner-content").removeClass("active");
+      $(anchor).addClass("active");
     });
+  });
 });
